e24f32ec5f53ad98c61756dd09f6486aff7a8478
Tintin <tanner@schenck.xyz>
2020-11-09 18:52:39 -0500

    initialize bootstrap and gut existing react content

---
+++ employee-directory/src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+// import SearchBox from "./components/SearchBox";
+
+
+function App() {
+  return (
+    <div>
+      <header>
+        <p>
+          Hello World
+        </p>
+
+      </ header>
+    </div>
+  );
+}
+
+export default App;
